a8f23e93ee41953774686d587efb250306aa411b
Rebeca Robbo <rebeca.lqa@gmail.com>
2022-03-23 16:46:06 -0300

    Imprimindo o tabuleiro colorido

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,10 +1,10 @@
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
-	
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -28,11 +28,8 @@ public class Position {
 
 	@Override
 	public String toString() {
-		
+
 		return row + ", " + column;
 	}
-	
-	
-	
-	
+
 }
